97815851292b0746369cb133957e83e1ffffc6de
LuisCardenasSolis <luiscardenas6498@gmail.com>
2020-05-28 03:04:59 -0500

    script v1

---
+++ dqueue.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SENDER="$1"
+RECIPIENT="$2"
+
+#si un parametro es vacio - ERROR
+if [[ -z $SENDER || -z $RECIPIENT ]]; then
+	echo "Error de Parametros -> 1. sender(all) - 2. recipient(all)"
+	exit 1
+elif [[ $SENDER = "all" ]]; then
+	VERIFY_PARAMS="\$8 ~ \"$RECIPIENT\""
+
+elif [[ $RECIPIENT = "all" ]]; then
+	VERIFY_PARAMS="\$7 == \"$SENDER\""
+
+else
+	VERIFY_PARAMS="(\$7 == \"$SENDER\") && (\$8 ~ \"$RECIPIENT\")"
+fi
+
+
+consult="/opt/zimbra/common/sbin/mailq | tail -n +2 | head -n -2 | grep -v '^ *(' | awk 'BEGIN { RS = \"\" } { if ( ${VERIFY_PARAMS} ) print \$1}' | tr -d '*!'"
+
+filter=$( eval "${consult}")
+
+for ID_Queue in $filter
+do
+	/opt/zimbra/common/sbin/postsuper -d $ID_Queue
+done
